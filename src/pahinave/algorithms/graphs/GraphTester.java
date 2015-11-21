ac34626ecaf298ef35fd72f480ec5dc7c9ce84e3
pahinave <ahinave.p@gmail.com>
2015-11-21 02:45:37 -0800

    graph contraction example for disconnected graph

--- src/pahinave/algorithms/graphs/GraphTester.java
+++ src/pahinave/algorithms/graphs/GraphTester.java
@@ -75,6 +75,37 @@ public class GraphTester {
 		graph.addEdge(new Edge<>(v4, v3, 7));
 		graph.addEdge(new Edge<>(v4, v1, 8));
 		graph.addEdge(new Edge<>(v2, v4, 9));
+		
+		
+		// NOTE2 - Marker start
+		// If this block is enabled and NOTE3 block is disabled
+		// then graph is disconnected into two groups
+		// v1 to v4 and v5 to v8. Program should output
+		// graph is disconnected, so no cut can be found
+		
+//		Vertex<Integer> v5 = graph.addVertex("5", 5);
+//		Vertex<Integer> v6 = graph.addVertex("6", 6);
+//		Vertex<Integer> v7 = graph.addVertex("7", 7);
+//		Vertex<Integer> v8 = graph.addVertex("8", 8);
+//
+//		graph.addEdge(new Edge<>(v5, v6, 10));
+//		graph.addEdge(new Edge<>(v6, v7, 11));
+//		graph.addEdge(new Edge<>(v8, v7, 12));
+//		graph.addEdge(new Edge<>(v8, v5, 13));
+//		graph.addEdge(new Edge<>(v6, v8, 14));
+		
+		//NOTE2 - Marker end
+		
+		//NOTE3 - Marker start
+		//If note 2 block is enabled and NOTE3 block
+		//is enabled, then below edge connects
+		//two disconnected graphs and you will find
+		//a cut
+		
+		//graph.addEdge(new Edge<>(v4, v5, 15));
+		
+		//NOTE3 - Marker end
+		
 
 		graph.showVertexEdgesView();
 
@@ -85,6 +116,10 @@ public class GraphTester {
 
 		System.out.println("Graph after contraction ...");
 		graph.showVertexEdgesView();
+		
+		if(graph.edgeCount() == 0) {
+			System.out.println("Graph is disconnected");
+		}
 
 		return graph;
 	}
