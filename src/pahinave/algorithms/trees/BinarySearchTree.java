3affaf184dc6bc01eed8739a6047b5f27e52b381
pahinave <ahinave.p@gmail.com>
2015-11-22 23:19:22 -0800

    Binary search tree

--- src/pahinave/algorithms/trees/BinarySearchTree.java
+++ src/pahinave/algorithms/trees/BinarySearchTree.java
@@ -1,16 +1,23 @@
 package pahinave.algorithms.trees;
 
+import java.util.List;
+
 public class BinarySearchTree<T extends Comparable<T>> {
 	Node root;
 
-	public class Node {
+	public class Node implements Comparable<Node> {
 		T data;
 		Node left;
 		Node right;
+		Node parent;
 
 		public Node(T data) {
 			super();
 			this.data = data;
+		}
+
+		public Node(Node value) {
+			this.setValues(value);
 		}
 
 		public T getData() {
@@ -37,16 +44,47 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			this.right = right;
 		}
 
+		public Node getParent() {
+			return parent;
+		}
+
+		public void setParent(Node parent) {
+			this.parent = parent;
+		}
+
 		@Override
 		public String toString() {
 			return "Node [data=" + data + "]";
 		}
 
+		@Override
+		public int compareTo(BinarySearchTree<T>.Node o) {
+			return this.getData().compareTo(o.getData());
+		}
+
+		public void setValues(Node value) {
+			this.data = value.getData();
+			this.left = value.getLeft();
+			this.right = value.getRight();
+			this.parent = value.getParent();
+		}
+
 	}
 
-	public void insert(T value) {
-		insert(new Node(value));
+	public Node getRoot() {
+		return root;
 	}
+
+	public void setRoot(Node root) {
+		this.root = root;
+	}
+
+	public Node insert(T value) {
+		Node newNode = new Node(value);
+		insert(newNode);
+		return newNode;
+	}
+
 	public void insert(Node newNode) {
 		if (root == null) {
 			root = newNode;
@@ -58,6 +96,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			if (newNode.getData().compareTo(current.getData()) <= 0) {
 				if (current.getLeft() == null) {
 					current.setLeft(newNode);
+					newNode.setParent(current);
 					break;
 				} else {
 					current = current.getLeft();
@@ -65,6 +104,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			} else {
 				if (current.getRight() == null) {
 					current.setRight(newNode);
+					newNode.setParent(current);
 					break;
 				} else {
 					current = current.getRight();
@@ -72,15 +112,15 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			}
 		}
 	}
-	
+
 	public Node search(T value) {
-		
+
 		Node current = root;
-		while(true) {
-			if(current == null || current.getData().compareTo(value) == 0) {
+		while (true) {
+			if (current == null || current.getData().compareTo(value) == 0) {
 				return current;
 			}
-			
+
 			if (value.compareTo(current.getData()) < 0) {
 				current = current.getLeft();
 			} else {
@@ -90,12 +130,20 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	}
 
 	public void printRecursive() {
-		if(root != null) {
+		if (root != null) {
 			System.out.println("debug printing recursive");
 			printRecursiveLoop(root, 0);
 		} else {
 			System.out.println("Tree is empty");
 		}
+	}
+
+	public void printPreorder(Node node) {
+		if (node == null)
+			return;
+		System.out.println(node);
+		printPreorder(node.getLeft());
+		printPreorder(node.getRight());
 	}
 
 	private void printRecursiveLoop(Node n, int level) {
@@ -108,6 +156,128 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		if (n.getRight() != null) {
 			printRecursiveLoop(n.getRight(), level + 1);
 		}
+	}
+
+	public void collectInorder(List<Node> inorder) {
+		collectInorder(inorder, this.getRoot());
+	}
+
+	public void collectInorder(List<Node> inorder, Node current) {
+		if (current.getLeft() != null) {
+			collectInorder(inorder, current.getLeft());
+		}
+		inorder.add(current);
+		if (current.getRight() != null) {
+			collectInorder(inorder, current.getRight());
+		}
+	}
+
+	// TODO: find predecessor
+
+	public Node maximum() {
+		if (root == null) {
+			return null;
+		}
+
+		return maximum(this.getRoot());
+	}
+
+	public Node maximum(Node node) {
+		while (node.getRight() != null) {
+			node = node.getRight();
+		}
+
+		return node;
+	}
+
+	public Node minimum() {
+		if (root == null) {
+			return null;
+		}
+
+		return minimum(this.getRoot());
+	}
+
+	public Node minimum(Node node) {
+		while (node.getLeft() != null) {
+			node = node.getLeft();
+		}
+
+		return node;
+	}
+
+	public Node predecessor(Node node) {
+
+		Node current = node;
+
+		if (current.getLeft() != null) {
+			return maximum(current.getLeft());
+		} else {
+
+			while (current.getParent() != null && current.getParent().getLeft() == current) {
+				current = current.getParent();
+			}
+
+			if (current == root) {
+				return null;
+			}
+
+			return current.getParent();
+		}
+
+	}
+
+	public Node successor(Node node) {
+		Node current = node;
+		if (current.getRight() != null) {
+			return minimum(current.getRight());
+		} else {
+			while (current.getParent() != null && current.getParent().getRight() == current) {
+				current = current.getParent();
+			}
+
+			if (current == root) {
+				return null;
+			}
+
+			return current.getParent();
+		}
+	}
+
+	public void delete(Node node) {
+
+		// simple case - node without any child
+		if (node.getLeft() == null && node.getRight() == null) {
+			if (node == root) {
+				root = null;
+			} else if (node.getParent().getLeft() == node) {
+				node.getParent().setLeft(null);
+			} else {
+				node.getParent().setRight(null);
+			}
+			return;
+		}
+
+		// node with single child
+		if (node.getLeft() == null || node.getRight() == null) {
+			Node singleChild = (node.getLeft() == null) ? node.getRight() : node.getLeft();
+			if (node == root) {
+				root = singleChild;
+			} else if (node.getParent().getLeft() == node) {
+				node.getParent().setLeft(singleChild);
+			} else {
+				node.getParent().setRight(singleChild);
+			}
+			return;
+		}
+
+		// node with two childs
+		Node pred = predecessor(node);
+		T temp = pred.getData();
+		pred.setData(node.getData());
+		node.setData(temp);
+		delete(pred);
+
 	}
 
 }
--- src/pahinave/algorithms/trees/tests/BinarySearchTreeTests.java
+++ src/pahinave/algorithms/trees/tests/BinarySearchTreeTests.java
@@ -1,35 +1,219 @@
 package pahinave.algorithms.trees.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import pahinave.algorithms.trees.BinarySearchTree;
 
 public class BinarySearchTreeTests {
+
+	BinarySearchTree<Integer> bst;
+	int range = 100;
+	int nNodes = 11;
+
+	@Before
+	public void initialize() {
+		Random r = new Random();
+
+		bst = new BinarySearchTree<>();
+
+		// tree assumes root node is 50
+		// so that random numbers will be distributed
+		// in both left and right tree
+		bst.insert(50);
+		for (int i = 0; i < nNodes - 1; i++) {
+			// all random numbers will be > 0
+			int n = r.nextInt(range) + 1;
+			bst.insert(n);
+		}
+	}
+
 	@Test
 	public void testInsert() throws Exception {
 		System.out.println("TEST CASE : testInsert");
-		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-		Random r = new Random();
-		for (int i = 0; i < 10; i++) {
-			bst.insert(r.nextInt(100));
-		}
+
 		bst.printRecursive();
 	}
 
 	@Test
 	public void testSearch() throws Exception {
 		System.out.println("TEST CASE : testSearch");
-		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
 		Random r = new Random();
-		for (int i = 0; i < 10; i++) {
-			bst.insert(r.nextInt(20));
-		}
-		bst.printRecursive();
 		for (int i = 0; i < 20; i++) {
-			Integer s = r.nextInt(20);
+			Integer s = r.nextInt(range);
 			System.out.println("Search " + s + " - Result: " + bst.search(s));
 		}
 	}
+
+	@Test
+	public void testCollectInorder() throws Exception {
+		System.out.println("TEST CASE : testCollectInorder");
+
+		List<BinarySearchTree<Integer>.Node> inorder = new ArrayList<>();
+		bst.collectInorder(inorder, bst.getRoot());
+		assertEquals(nNodes, inorder.size());
+		for (int i = 0; i < nNodes - 1; i++) {
+			assertNotEquals(1, inorder.get(i).compareTo(inorder.get(i + 1)));
+		}
+
+	}
+
+	@Test
+	public void testPredecessor() throws Exception {
+		System.out.println(">>>>>>>>> TEST CASE : testPredecessor");
+
+		List<BinarySearchTree<Integer>.Node> inorder = new ArrayList<>();
+		bst.collectInorder(inorder, bst.getRoot());
+
+		assertEquals("predecessor of min node is not null", null, bst.predecessor(inorder.get(0)));
+		for (int i = 1; i < inorder.size(); i++) {
+			assertEquals("predecessor is wrong", inorder.get(i - 1), bst.predecessor(inorder.get(i)));
+		}
+	}
+
+	@Test
+	public void testFindMax() throws Exception {
+		// insert max number
+		int max = range + 1;
+		bst.insert(max);
+		BinarySearchTree<Integer>.Node foundMax = bst.maximum();
+		assertEquals(Integer.valueOf(max), foundMax.getData());
+	}
+
+	@Test
+	public void findMinium() throws Exception {
+		// insert min number
+		int min = 0;
+		bst.insert(min);
+		BinarySearchTree<Integer>.Node foundMin = bst.minimum();
+		assertEquals(Integer.valueOf(min), foundMin.getData());
+	}
+
+	@Test
+	public void findMinimumInEmptyTree() throws Exception {
+		assertEquals(null, new BinarySearchTree<Integer>().minimum());
+	}
+
+	@Test
+	public void findMaximumInEmptyTree() throws Exception {
+		assertEquals(null, new BinarySearchTree<Integer>().maximum());
+	}
+
+	@Test
+	public void testSuccessor() throws Exception {
+		List<BinarySearchTree<Integer>.Node> inorder = new ArrayList<>();
+		bst.collectInorder(inorder, bst.getRoot());
+
+		for (int i = 0; i < inorder.size() - 1; i++) {
+			assertEquals(inorder.get(i + 1), bst.successor(inorder.get(i)));
+		}
+		assertEquals(null, bst.successor(inorder.get(inorder.size() - 1)));
+
+	}
+
+	@Test
+	public void testDeleteSimple() throws Exception {
+		bst = new BinarySearchTree<>();
+
+		BinarySearchTree<Integer>.Node node5 = bst.insert(5);
+		BinarySearchTree<Integer>.Node node3 = bst.insert(3);
+		BinarySearchTree<Integer>.Node node6 = bst.insert(6);
+		BinarySearchTree<Integer>.Node node4 = bst.insert(4);
+
+		bst.delete(node4);
+		List<BinarySearchTree<Integer>.Node> nodes = new ArrayList<>();
+		bst.collectInorder(nodes);
+		assertEquals(3, nodes.size());
+		assertEquals(node3, nodes.get(0));
+		assertEquals(node5, nodes.get(1));
+		assertEquals(node6, nodes.get(2));
+
+	}
+
+	@Test
+	public void testDeleteSingleChild1() throws Exception {
+
+		// single child is right of parent
+		bst = new BinarySearchTree<>();
+
+		BinarySearchTree<Integer>.Node node5 = bst.insert(5);
+		BinarySearchTree<Integer>.Node node3 = bst.insert(3);
+		BinarySearchTree<Integer>.Node node6 = bst.insert(6);
+		BinarySearchTree<Integer>.Node node4 = bst.insert(4);
+		BinarySearchTree<Integer>.Node node1 = bst.insert(1);
+		BinarySearchTree<Integer>.Node node2 = bst.insert(2);
+
+		bst.delete(node2);
+		List<BinarySearchTree<Integer>.Node> nodes = new ArrayList<>();
+		bst.collectInorder(nodes);
+		assertEquals(5, nodes.size());
+		assertEquals(node1, nodes.get(0));
+		assertEquals(node3, nodes.get(1));
+		assertEquals(node4, nodes.get(2));
+		assertEquals(node5, nodes.get(3));
+		assertEquals(node6, nodes.get(4));
+	}
+
+	@Test
+	public void testDeleteSingleChild2() throws Exception {
+
+		// single child is left of parent
+		bst = new BinarySearchTree<>();
+
+		BinarySearchTree<Integer>.Node node5 = bst.insert(5);
+		BinarySearchTree<Integer>.Node node3 = bst.insert(3);
+		BinarySearchTree<Integer>.Node node6 = bst.insert(6);
+		BinarySearchTree<Integer>.Node node4 = bst.insert(4);
+		BinarySearchTree<Integer>.Node node2 = bst.insert(2);
+		BinarySearchTree<Integer>.Node node1 = bst.insert(1);
+
+		bst.delete(node2);
+		List<BinarySearchTree<Integer>.Node> nodes = new ArrayList<>();
+		bst.collectInorder(nodes);
+		assertEquals(5, nodes.size());
+		assertEquals(node1, nodes.get(0));
+		assertEquals(node3, nodes.get(1));
+		assertEquals(node4, nodes.get(2));
+		assertEquals(node5, nodes.get(3));
+		assertEquals(node6, nodes.get(4));
+	}
+
+	@Test
+	public void testNodeWithTwoChilds() throws Exception {
+		// single child is left of parent
+		bst = new BinarySearchTree<>();
+
+		// 5
+		// x3 6
+		// 2 4
+		// 1
+		// 0
+		bst.insert(5);
+		BinarySearchTree<Integer>.Node nodeToDelete = bst.insert(3);
+		bst.insert(6);
+		bst.insert(4);
+		bst.insert(2);
+		bst.insert(1);
+		bst.insert(0);
+
+		bst.delete(nodeToDelete);
+		List<BinarySearchTree<Integer>.Node> nodes = new ArrayList<>();
+		bst.collectInorder(nodes);
+		assertEquals(6, nodes.size());
+		assertEquals(Integer.valueOf(0), nodes.get(0).getData());
+		assertEquals(Integer.valueOf(1), nodes.get(1).getData());
+		assertEquals(Integer.valueOf(2), nodes.get(2).getData());
+		assertEquals(Integer.valueOf(4), nodes.get(3).getData());
+		assertEquals(Integer.valueOf(5), nodes.get(4).getData());
+		assertEquals(Integer.valueOf(6), nodes.get(5).getData());
+
+	}
+
 }
